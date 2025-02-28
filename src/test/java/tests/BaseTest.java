7b8e02811a572e8d9a2ab7bcb0ff7f9a0013f06b
Anastasiya <email>
2025-02-28 18:35:22 +0300

    refactor

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,47 +1,48 @@
 package tests;
 
-import constants.IConstants;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import listeners.TestListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import pages.*;
+import steps.LoginSteps;
+import steps.ProductSteps;
 
 import java.util.concurrent.TimeUnit;
 
 @Listeners(TestListener.class)
-public class BaseTest implements IConstants, ITestConstants {
+public class BaseTest implements ITestConstants {
     WebDriver driver;
     LoginPage loginPage;
     ProductsPage productsPage;
     CartPage cartPage;
-    HeaderPage headerPage;
-    LoginPageFactory loginPageFactory;
+    ProductSteps productSteps;
+    LoginSteps loginSteps;
 
-    /**
-     * This is initialization of pages
-     */
     @BeforeMethod
-    public void initTest() {
+    public void initTest(ITestContext iTestContext) {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         initPages();
+        PageFactory.initElements(driver, this);
     }
 
     public void initPages() {
         loginPage = new LoginPage(driver);
         productsPage = new ProductsPage(driver);
         cartPage = new CartPage(driver);
-        headerPage = new HeaderPage(driver);
-        loginPageFactory = new LoginPageFactory(driver);
+        productSteps = new ProductSteps(driver);
+        loginSteps = new LoginSteps(driver);
     }
 
-    @AfterMethod
+    @AfterMethod(alwaysRun = true)
     public void endTest() {
         driver.quit();
     }
